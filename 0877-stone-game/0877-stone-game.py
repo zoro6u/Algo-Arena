5bcffa0c36549cdca91237b713fd3421d3bbed3a
Zoro Mustafa <mm4051103@gmail.com>
2026-08-02 12:42:19 +0200

    Time: 0 ms (100%), Space: 19.2 MB (75.06%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,10 +1,3 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        n = len(piles)
-        dp = piles[:]  # dp[j] represents dp[i][j] for current row i
-
-        for i in range(n - 2, -1, -1):
-            for j in range(i + 1, n):
-                dp[j] = max(piles[i] - dp[j], piles[j] - dp[j - 1])
-
-        return dp[n - 1] > 0
+        return True
