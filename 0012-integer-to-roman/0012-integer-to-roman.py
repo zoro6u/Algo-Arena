5b2a314253764bd47ca515456a503f501283bdfa
Zoro Mustafa <mm4051103@gmail.com>
2026-08-07 14:02:15 +0200

    Time: 7 ms (37.96%), Space: 19.3 MB (25.1%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,10 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        thousands = ["", "M", "MM", "MMM"]
+        hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        return (thousands[num // 1000]
+                + hundreds[(num // 100) % 10]
+                + tens[(num // 10) % 10]
+                + ones[num % 10])
